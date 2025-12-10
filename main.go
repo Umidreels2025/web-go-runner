a81416ae93a4f981c1619522e288bd9efe72d9af
Umidjon Mamadjanov <umidjonmamadjanov653@gmail.com>
2025-12-10 21:45:31 +0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+    "bytes"
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "os"
+    "os/exec"
+)
+
+func runHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost {
+        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+        return
+    }
+
+    code := r.FormValue("code")
+    tmpFile := "temp_code.go"
+    ioutil.WriteFile(tmpFile, []byte(code), 0644)
+
+    cmd := exec.Command("go", "run", tmpFile)
+    var outBuf, errBuf bytes.Buffer
+    cmd.Stdout = &outBuf
+    cmd.Stderr = &errBuf
+
+    err := cmd.Run()
+    output := outBuf.String()
+    errOutput := errBuf.String()
+
+    if err != nil && errOutput == "" {
+        errOutput = err.Error()
+    }
+
+    fmt.Fprintf(w, `{"output":%q, "error":%q}`, output, errOutput)
+    os.Remove(tmpFile)
+}
+
+func main() {
+    fs := http.FileServer(http.Dir("./"))
+    http.Handle("/", fs)
+    http.HandleFunc("/run", runHandler)
+
+    fmt.Println("Server running on :8080")
+    http.ListenAndServe(":8080", nil)
+}
